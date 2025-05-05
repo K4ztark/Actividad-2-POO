b82df9ad4412c78ed9ce0f277c8763205807c078
k4ztark <jucastanedah@unal.edu.co>
2025-05-04 22:00:59 -0500

    Segundo commit punto 5

---
+++ Actividad2_Ejercicio1/Main.java
@@ -0,0 +1,10 @@
+package Actividad2_Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Pedro", "Pérez", "1053121010",1998,"Colombia",'H');
+        Persona p2 = new Persona("Luis", "León", "1053223344", 2001,"Uruguay",'H');
+        p1.imprimir();
+        p2.imprimir();
+    }
+}
